429baeb71deedcaba835e379e236b23f4b0fbafb
konstantinrjab <konstantinrjab@gmail.com>
2019-11-02 20:12:07 +0200

    installed adminLTE

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,40 +1,53 @@
 @extends('layouts.login')
 
-@section('title', 'Login Page')
+<div class="login-box">
+    <div class="login-logo">
+        <a href="/"><b>Lumy</b>App</a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="card">
+        <div class="card-body login-card-body">
+            <p class="login-box-msg">Sign in to start your session</p>
 
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-                <div class="card card-signin my-5">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Sign In</h5>
-                        <form class="form-signin" method="post">
-                            {!! csrf_field() !!}
+            <form action="" method="post">
+                {!! csrf_field() !!}
 
-                            @if (count($errors) > 0)
-                                <ul class="alert alert-danger">
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            @endif
+                @if (count($errors) > 0)
+                    <ul class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
 
-                            <div class="form-label-group">
-                                <label for="inputEmail">Email</label>
-                                <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email" required="" autofocus="" value="{{ old('email') }}">
-                            </div>
-
-                            <div class="form-label-group mt-2">
-                                <label for="inputPassword">Password</label>
-                                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required="">
-                            </div>
-
-                            <button class="btn btn-primary btn-block mt-3" type="submit">Sign in</button>
-                        </form>
+                <div class="input-group mb-3">
+                    <input type="email" name="email" class="form-control" placeholder="Email" required="" autofocus="" value="{{ old('email') }}">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div class="input-group mb-3">
+                    <input type="password" name="password" class="form-control" placeholder="Password" required="">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-8">
+                    </div>
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+
         </div>
+        <!-- /.login-card-body -->
     </div>
-@stop
+</div>
+<!-- /.login-box -->
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,29 +1,22 @@
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Log in</title>
+    <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"--}}
-    {{--          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">--}}
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-            crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="/dist/css/adminlte.min.css">
-    <title>App Name - @yield('title')</title>
+
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="/vendor/adminlte/dist/css/adminlte.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<body class="hold-transition login-page">
 
-    @section('sidebar')
+@yield('content')
 
-    @show
-
-    @yield('content')
-</div>
 </body>
 </html>
