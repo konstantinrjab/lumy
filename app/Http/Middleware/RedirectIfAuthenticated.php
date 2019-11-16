04a5c62180fcc0aa1ece40279846ca18d64a858f
konstantinrjab <konstantinrjab@gmail.com>
2019-11-16 15:59:17 +0200

    redirect if authenticated fix

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/');
+            return redirect()->to(env('FRONTEND_URL'));
         }
 
         return $next($request);
