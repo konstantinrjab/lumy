c4a9be43ea473bfa2f06ad1581c09429899af0bc
k.riabchenko <Xd1NVmava3gs!>
2019-12-26 21:41:40 +0200

    deal price nominal fix

--- app/Http/Requests/DealStoreRequest.php
+++ app/Http/Requests/DealStoreRequest.php
@@ -27,7 +27,7 @@ class DealStoreRequest extends FormRequest
             'title'                     => 'required|string|max:100',
             'status'                    => 'nullable|string|max:100|in:' . implode(',', DealStatusEnum::getValues()),
             'clientId'                  => 'nullable|integer', 'exists:clients,id',
-            'price.nominal'             => 'nullable|numeric|required_with:price.currency|between:0,999999999.99',
+            'price.nominal'             => 'nullable|numeric|between:0,999999999.99',
             'price.currency'            => 'nullable|string|required_with:price.nominal|in:' . implode(',', CurrencyEnum::getValues()),
             'prepay.nominal'            => 'nullable|numeric|between:0,999999999.99',
             'prepay.currency'           => 'nullable|string|in:' . implode(',', CurrencyEnum::getValues()),
